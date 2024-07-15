bc436c58e1322e6f9c5dff91351de35974d4c72f
Dmitrii Bulatov <bulati484@gmail.com>
2024-07-15 19:23:07 +0300

    db changed

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    user: 'bczbngvg',
+    host: 'flora.db.elephantsql.com',
+    database: 'bczbngvg',
+    password: 'BGq6InRZA7itkELFSX48IHS5ZuIZ5JLx',
+    port: 5432,
 });
 
 module.exports = pool;
